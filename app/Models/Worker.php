2e5eb42f42c8042cbca9f881f8346f6f6b400af6
Jason Wu <jason_wu@aai.com.tw>
2023-07-12 14:29:04 +0800

    Add Worker CRUD API

---
+++ app/Models/Worker.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Worker extends Model
+{
+    use HasFactory;
+    // const CREATED_AT = 'creation_date';
+    // const UPDATED_AT = 'last_update';
+
+}
